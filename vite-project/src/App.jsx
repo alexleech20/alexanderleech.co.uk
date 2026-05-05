264d9fb3ebc7ab51b1c36230461f286050782ff2
alexander.leech <alexander.leech@capita.com>
2026-05-05 23:15:00 +0100

    importing homepage component into app.jsx, next step will be to install tailwind js

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,14 +1,18 @@
-import { useState } from 'react'
 import './App.css'
+
+// component imports
 import Background from './components/background'
+import HomePage from './components/homepage'
 
 function App() {
 
   return (
     <>
       <Background />
+      
       <main style={{ position: "relative", zIndex: 1 }}>
-        <h1 stlye={{ color: "white" }}>Hello people</h1>
+        <HomePage />
+        <h1>Hello people</h1>
       </main>
     </>
   )
