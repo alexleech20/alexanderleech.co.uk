222e07662014f734c0f34396fb58a4627f70c973
alexander.leech <alexander.leech@capita.com>
2026-05-06 00:09:38 +0100

    playting round with tailwind columns, got 50/50 split setup to begin development of content tomorrow.

--- vite-project/src/components/homepage.jsx
+++ vite-project/src/components/homepage.jsx
@@ -3,13 +3,16 @@ import react from 'react';
 
 export default function HomePage() {
 
-
   return (
     // This component will house all sub components in one central location
     <>
-      <p>
-        I will now contain all homepage content 
-      </p>
+      <div class="grid grid-cols-2 gap-4 pt-5">
+        {/* <leftHalfComponent /> */}
+        <div>split 50/50</div>
+        
+        {/* <rightHalfComponent /> */}
+        <div>split 50/50</div>
+      </div>
     </>
   )
 }
