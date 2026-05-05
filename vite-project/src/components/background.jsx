165cbbeb0fa8ea3486502e5c1b56ffe489bcfda0
alexander.leech <alexander.leech@capita.com>
2026-05-05 22:55:42 +0100

    highlighting now sits behiund content no longer blocking user clicks, decoupled the tracking from the element to fix the issue. tracks mouse on window now

--- vite-project/src/components/background.jsx
+++ vite-project/src/components/background.jsx
@@ -3,24 +3,34 @@ import { useEffect, useRef } from "react";
 export default function Background() {
   const ref = useRef(null)
 
-  const handleMouseMove = (e) => {
-    if (!ref.current) return;
+  useEffect(() => {
+    const handleMouseMove = (e) => {
+      if (!ref.current) return;
 
-    const x = (e.clientX / window.innerWidth) * 100;
-    const y = (e.clientY / window.innerHeight) * 100;
+      const x = (e.clientX / window.innerWidth) * 100;
+      const y = (e.clientY / window.innerHeight) * 100;
 
-    ref.current.style.setProperty("--x", `${x}%`);
-    ref.current.style.setProperty("--y", `${y}%`)
-  };
+      ref.current.style.setProperty("--x", `${x}%`);
+      ref.current.style.setProperty("--y", `${y}%`)
+    };
+
+    window.addEventListener("mousemove", handleMouseMove);
+
+    return () => {
+      window.removeEventListener("mousemove", handleMouseMove);
+    };
+  }, []);
+  
 
   return (
     <div
       ref={ref}
-      onMouseMove={handleMouseMove}
       style={{
         position: "fixed",
         inset: 0,
-
+        zIndex: 0,
+        pointerEvents: "none",
+        
         // defaulted centre position
         "--x": "50%",
         "--y": "50%",
