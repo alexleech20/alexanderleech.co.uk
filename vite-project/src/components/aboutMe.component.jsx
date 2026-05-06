52ea5d579bb5e491216b9058447b91a4d15f28f7
alexander.leech <alexander.leech@capita.com>
2026-05-06 18:29:50 +0100

    added an aboutme component, inserted into appjsx, also rejigged apdding on app jsx

---
+++ vite-project/src/components/aboutMe.component.jsx
@@ -0,0 +1,23 @@
+import react from 'react';
+
+
+export default function AboutMe() {
+
+
+  return (
+    <>
+      <p>I'm Alexander, a frontend engineer who focuses on building accessible, well-crafted user interfaces. I care a lot about the details and enjoy creating products that feel intuitive and inclusive. I’m at my best working between design and engineering, turning good ideas into clean, maintainable code.</p>
+      <br />
+      <p>I’m currently working on the Live Services team at Capita, we're a tight-knit team who look after and grow products once they’re out in the real world. It means working across different stacks and clients, and making sure everything stays stable, relevant, and built to last.</p>
+      <br />
+      <p>Previously, I have worked across a wide range of environments - from startups to large scale consultancies 
+        - including 
+        <a target="_blank" href="https://www.capita.com/"> Capita</a>, 
+        <a target="_blank" href="https://www.linkedin.com/company/orange-bus-ltd/about/"> Orangebus</a> & 
+        <a target="_blank" href="https://www.linkedin.com/company/daykin-&-storey/?originalSubdomain=uk"> Daykin & Storey.</a>
+      </p>
+      <br />
+      <p>In my spare time, you will often find me at the gym, walking my dog, or watching the football (Newcastle).</p>
+    </>
+  )
+} 
--- vite-project/src/components/homepage.jsx
+++ vite-project/src/components/homepage.jsx
@@ -1,4 +1,5 @@
 import react from 'react';
+import AboutMe from './aboutMe.component';
 
 
 export default function HomePage() {
@@ -6,18 +7,18 @@ export default function HomePage() {
   return (
     // This component will house all sub components in one central location
     <>
-      {/* tailwind column system works in tell what column to use for mobile as default first  */}
-      {/*  then tell what to use 'sm up' */}
-      <div class="grid grid-cols-2 gap-4 pt-5">
+      <div class="grid grid-cols-1 md:grid-cols-2 gap-4 pt-5">
         {/* <leftHalfComponent /> */}
-        <div class="pt-10 pl-10 text-left">
+        <div class="grid pt-10 px-5 text-left">
           <h1>Alexander Leech</h1>
           <h2 class="pt-3 pb-3">Frontend Developer</h2>
           <p>I build accessible, mobile first experiences for users.</p>
         </div>
         
         {/* <rightHalfComponent /> */}
-        <div>split 50/50</div>
+        <div class="grid px-5">
+          <AboutMe />
+        </div>
       </div>
     </>
   )
