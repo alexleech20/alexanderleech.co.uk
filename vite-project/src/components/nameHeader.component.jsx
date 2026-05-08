d20cad4efa343baf216a43bbe2d96c0688a10539
alexander.leech <alexander.leech@capita.com>
2026-05-08 18:00:20 +0100

    split the name header into its own component, better organisation#

---
+++ vite-project/src/components/nameHeader.component.jsx
@@ -0,0 +1,14 @@
+import react from 'react';
+
+
+export default function NameHeader() {
+
+
+  return (
+    <>
+      <h1>Alexander Leech</h1>
+      <h2 class="pt-3 pb-3">Frontend Developer</h2>
+      <p>I build accessible, mobile first experiences for users.</p>
+    </>
+  )
+} 
