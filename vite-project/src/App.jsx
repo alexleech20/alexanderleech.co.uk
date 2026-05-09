163868d2502a412c8234cbc68eeec85b2fd2c38a
Alex Leech <alexleech546@gmail.com>
2026-05-10 00:51:51 +0100

    added react helmet

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -3,11 +3,17 @@ import './App.css'
 // component imports
 import Background from './components/background'
 import HomePage from './components/homepage'
+import {Helmet} from 'react-helmet';
+
 
 function App() {
 
   return (
     <>
+      <Helmet>
+        <title>Alexander Leech - Frontend Developer</title>
+        <meta name="description"/>
+      </Helmet>
       <Background />
       
       <main style={{ position: "relative", zIndex: 1 }}>
