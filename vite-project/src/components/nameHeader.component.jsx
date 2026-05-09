1faae2870f24f1434afb913f09f2f23cdbbd099c
Alex Leech <alexleech546@gmail.com>
2026-05-09 18:26:08 +0100

    added aria labels to what needs for now, screenreader performance should improve

--- vite-project/src/components/nameHeader.component.jsx
+++ vite-project/src/components/nameHeader.component.jsx
@@ -7,12 +7,13 @@ export default function NameHeader() {
 
   return (
     <>
-      <h1>{nameHeader}</h1>
+      <h1 aria-label="Alexander Leech Header">{nameHeader}</h1>
       <h2 class="pt-3 pb-3">Frontend Developer</h2>
       <p>I build accessible, mobile first experiences for users.</p>
       <div className="responsiveMediaPadding" style={{ display: 'flex', flexDirection: 'row'}}>
         <a 
           href="https://github.com/alexanderleech" 
+          aria-label="GitHub Profile Link"
           target="_blank" 
           rel="noopener noreferrer" 
           style={{ paddingRight: '1.5rem'}}
@@ -21,6 +22,7 @@ export default function NameHeader() {
         </a>
         <a 
           href="https://www.linkedin.com/in/alexander-l-96889817b/" 
+          aria-label="LinkedIn Profile Link"
           target="_blank" 
           rel="noopener noreferrer"
           style={{ paddingRight: '1.5rem'}}
@@ -29,6 +31,7 @@ export default function NameHeader() {
         </a>
         <a 
           href="https://www.codepen.io/Alex-Leech" 
+          aria-label="CodePen Profile Link"
           target="_blank" 
           rel="noopener noreferrer"
         >
