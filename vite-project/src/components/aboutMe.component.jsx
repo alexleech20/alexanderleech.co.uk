33e18bd772d0ed7fbb574203e6df8b387001cf08
Alex Leech <alexleech546@gmail.com>
2026-05-11 17:43:18 +0100

    made a hover state class for anchors rather than targeting all anchors the same.

--- vite-project/src/components/aboutMe.component.jsx
+++ vite-project/src/components/aboutMe.component.jsx
@@ -10,9 +10,9 @@ export default function AboutMe() {
       <br />
       <p>Previously, I have worked across a wide range of environments - from startups to large scale consultancies 
         - including 
-        <a aria-label="Link to capita.com" target="_blank" href="https://www.capita.com/"> Capita</a>, 
-        <a aria-label="Link to orangebus linkedin page" target="_blank" href="https://www.linkedin.com/company/orange-bus-ltd/about/"> Orangebus</a> & 
-        <a aria-label="Link to daykin-and-storeys linkedin page" target="_blank" href="https://www.linkedin.com/company/daykin-&-storey/?originalSubdomain=uk"> Daykin & Storey.</a>
+        <a className="anchorHoverStyles" aria-label="Link to capita.com" target="_blank" href="https://www.capita.com/"> Capita</a>, 
+        <a className="anchorHoverStyles" aria-label="Link to orangebus linkedin page" target="_blank" href="https://www.linkedin.com/company/orange-bus-ltd/about/"> Orangebus</a> & 
+        <a className="anchorHoverStyles" aria-label="Link to daykin-and-storeys linkedin page" target="_blank" href="https://www.linkedin.com/company/daykin-&-storey/?originalSubdomain=uk"> Daykin & Storey.</a>
       </p>
       <br />
       <p>In my spare time, you will often find me at the gym, walking my dog, or watching the football (Newcastle).</p>
